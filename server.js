76e9a7f7cc8043f1f3e213073070b2da633456e8
Lounes Dupuis-Dabaji <dupuisdl@iut1.u-ga.fr>
2024-01-15 11:48:14 +0100

    add websocket server side

--- server.js
+++ server.js
@@ -4,3 +4,28 @@ app.use(express.static(__dirname + '/public')); //__dir and not _dir
 var port = 8000; // you can use any port
 app.listen(port);
 console.log('server on http://localhost:' + port + '/');
+
+// -------------------------------------------
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 7071 });
+
+const clients = new Map();
+
+wss.on('connection', (ws) => {
+    const id = uuidv4();
+    const metadata = { id };
+
+    clients.set(ws, metadata);
+})
+
+ws.on("close", () => {
+    clients.delete(ws);
+});
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
